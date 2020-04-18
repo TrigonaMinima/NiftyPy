86c0e287c58b772bba5a0df66ded568b42e1f624
TrigonaMinima <shivamrana95@yahoo.in>
2020-04-18 18:22:18 +0530

    Added method to syntax highlight the exceptions in interpreter

--- nifty/__init__.py
+++ nifty/__init__.py
@@ -1,1 +1,3 @@
 from .main import *
+
+set_highlighted_excepthook()
--- nifty/main.py
+++ nifty/main.py
@@ -31,3 +31,20 @@ def ls(path=""):
     print("\n".join(sorted([i.as_posix() for i in paths])))
     return paths
 
+
+def set_highlighted_excepthook():
+    import sys
+    import traceback
+    from pygments import highlight
+    from pygments.lexers import get_lexer_by_name
+    from pygments.formatters import TerminalFormatter
+
+    lexer = get_lexer_by_name(
+        "pytb" if sys.version_info.major < 3 else "py3tb")
+    formatter = TerminalFormatter()
+
+    def myexcepthook(type, value, tb):
+        tbtext = ''.join(traceback.format_exception(type, value, tb))
+        sys.stderr.write(highlight(tbtext, lexer, formatter))
+
+    sys.excepthook = myexcepthook
